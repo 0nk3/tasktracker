d7ca354e4fc68aa34a8d6a56c5676c7f8ffccfa2
onke <0nk3.0nk3@gmail.com>
2024-05-23 20:04:04 +0200

    create views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_new(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        task = Task(title=title, description=description)
+        task.save()
+    return render(request, 'tasks/task_new.html')
+    
+def task_edit(request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        task.title = title
+        task.description = description
+        task.save()
+        redirect('task_list')
+    return render(request, 'tasks/task_edit.html', {'task': task})
+
+def task_delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+    return render(request, 'tasks/task_delete.html')
+
+def task_complete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.completed = True
+    task.save()
+    return render(request, 'tasks/task_complete.html')
+
+def task_delete_completed(request):
+    Task.objects.filter(completed=True).delete()
+    return render(request, 'tasks/task_delete_completed.html')
+
+def task_delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+    return redirect('task_list')
